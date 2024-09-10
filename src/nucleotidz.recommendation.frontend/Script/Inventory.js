7593ad556e541b5ea4accf8f7c65e724c2c287c9
ahmar-husain <ahmar.husain@maersk.com>
2024-09-10 10:52:33 +0530

    Upload

---
+++ src/nucleotidz.recommendation.frontend/Script/Inventory.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function () {
+    $('#file-upload').change(function () {
+        var fileName = $(this).val().split('\\').pop();
+        $('#file-selected').text(fileName ? fileName : 'No file selected');
+    });
+        function showToast() {
+            const toastContainer = document.getElementById('toast-container');
+            toastContainer.classList.remove('hidden', 'toast-exit');
+            toastContainer.classList.add('toast-enter');
+            setTimeout(() => {
+                toastContainer.classList.remove('toast-enter');
+                toastContainer.classList.add('toast-exit');
+                setTimeout(() => {
+                    toastContainer.classList.add('hidden');
+                }, 15000); // Match the duration of the exit animation
+            }, 500); // Show toast for 3 seconds
+        }
+        $('#upload-button').click(function() {
+            var fileInput = $('#file-upload')[0];
+            var file = fileInput.files[0];
+            
+            if (!file) {
+                alert('Please select a file to upload.');
+                return;
+            }
+
+            var formData = new FormData();
+            formData.append('file', file);
+
+            $.ajax({
+                url: api + "/Product/bulk",
+                type: 'POST',
+                data: formData,
+                contentType: false,
+                processData: false,
+                success: function(response) {
+        showToast();
+                },
+                error: function(xhr, status, error) {
+                    alert('File upload failed. Please try again.');
+                }
+            });
+        });
+    });
+
--- src/nucleotidz.recommendation.frontend/Script/Variable.js
+++ src/nucleotidz.recommendation.frontend/Script/Variable.js
@@ -1,1 +1,1 @@
-var api = "https://localhost:49189/api";
+var api = "https://localhost:49193/api";
